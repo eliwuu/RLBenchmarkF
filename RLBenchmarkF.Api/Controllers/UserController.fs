ddcb976c50d094c687f332daa87c6ca839d7e6db
Eliwuu <marek.wilk@gmail.com>
2022-03-10 22:28:23 +0100

    Initial user controller

---
+++ RLBenchmarkF.Api/Controllers/UserController.fs
@@ -0,0 +1,23 @@
+﻿namespace RLBenchmarkF.Api.Controllers
+
+open RLBenchmarkF.Models
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open RLBenchmarkF.Api
+
+
+[<ApiController>]
+[<Route("[controller]")>]
+
+type UserController (logger : ILogger<UserController>) =
+    inherit ControllerBase()
+
+    [<HttpPost("create")>]
+    member _.Create(registerDto : User.RegisterDto) = 
+        match User.exist(registerDto.Email) with 
+            | false -> User.create(registerDto) |> JsonResult
+            | true -> JsonResult($"User already exist")

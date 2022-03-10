95390e211e438a37a1fb417d24fffcbcdb7d9dfc
Eliwuu <marek.wilk@gmail.com>
2022-03-10 22:29:24 +0100

    Db connection string

--- RLBenchmarkF.Data/Db.fs
+++ RLBenchmarkF.Data/Db.fs
@@ -1,5 +1,26 @@
 ﻿namespace RLBenchmark.Data
 
-module internal Db = 
-    let a = 1
+open Microsoft.Extensions.Configuration
+open System.IO
 
+open MongoDB.Bson
+open MongoDB.Driver
+open System.IO
+
+module AppSettings = 
+    let configuration = 
+        ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build()
+
+module internal Db = 
+    let connectionString = 
+        AppSettings
+            .configuration
+            .GetConnectionString("db");
+
+    let settings = connectionString.Replace("myFirstDatabase", "RLDB") |> MongoClientSettings.FromConnectionString
+    settings.ServerApi <- ServerApiVersion.V1 |> ServerApi
+
+    let client = settings |> MongoClient
--- RLBenchmarkF.Data/Source.fs
+++ RLBenchmarkF.Data/Source.fs
@@ -1,10 +1,30 @@
 ﻿namespace RLBenchmarkF.Data
 
 open System
+open RLBenchmark.Data
+open MongoDB.Driver
+open System.Collections.Generic
 
-module Source = 
-    let db = 1
-    let get<'T> (sourceName: string, predicate: Func<'T, bool>) = db
-    let set<'T> (sourceName: string, data: 'T) = db
-    let update<'T> (sourceName: string, predicate: Func<'T, bool>, data: 'T ) = db
-    let delete<'T> (sourceName: string, predicate: Func<'T, bool>) = db
+module Collection = 
+
+    let db = Db.client.GetDatabase("RLDB")
+    let get<'T> (collectionName: string, predicate: FilterDefinition<'T>) = 
+        db
+            .GetCollection(collectionName)
+            .FindAsync(predicate)
+    let insertMany<'T> (collectionName: string, data: IEnumerable<'T>) = 
+        db
+            .GetCollection(collectionName)
+            .InsertManyAsync(data)
+
+    let insertOne<'T> (collectionName: string, data: 'T) =
+        db
+            .GetCollection(collectionName)
+            .InsertOneAsync(data)
+    let update<'T> (collectionName: string, predicate: FilterDefinition<'T>, data: 'T ) = 
+        match typeof(IEnumerable) with
+            | IsAssignableFrom
+        db
+            .GetCollection(collectionName)
+            
+    let delete<'T> (collectionName: string, predicate: Func<'T, bool>) = db
